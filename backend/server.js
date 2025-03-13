733f9ff0e4547ce625447db81d4562453e8ad8ec
ahmad <mk4249796@gmail.com>
2025-03-13 12:36:52 +0200

    modiffied somthing

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
-/*import express from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // ✅ Fix: Enable CORS before defining routes
 app.use(cors({ origin: "http://localhost:5173", methods: "GET,POST,PUT,DELETE" }));
@@ -17,12 +20,19 @@ app.use(express.json());
 // ✅ Define routes after middleware
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 // ✅ Start server
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server started at http://localhost:${PORT}`);
-});*/
-import express from "express";
+});
+/*import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
@@ -51,4 +61,4 @@ if (process.env.NODE_ENV === "production") {
 app.listen(PORT, () => {
 	connectDB();
 	console.log("Server started at http://localhost:" + PORT);
-});
+});*/
